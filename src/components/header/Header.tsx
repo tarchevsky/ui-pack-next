b1bcc72abc0847c79c3038c5259e380a01476b20
tarchevsky <i.tarchevsky@gmai.com>
2024-04-02 13:32:48 +0300

    Проверка работы vercel 2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<header>
 			<div className='flex justify-evenly md:justify-between items-center py-4 fade-in'>
-				<Link href='/'>LOGO 1</Link>
+				<Link href='/'>LOGO 2</Link>
 				<nav>
 					<ul
 						tabIndex={0}
