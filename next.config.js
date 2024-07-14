fd89f2d35e8f3be6659dd28fc8c80e4bf5294b64
tarchevsky <i.tarchevsky@gmai.com>
2024-07-14 21:31:22 +0300

    обновление конфига next.config с разрешенными доменами внешних картинок - новый способ

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['images.unsplash.com']
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+				port: ''
+			}
+		]
 	}
 }
 
