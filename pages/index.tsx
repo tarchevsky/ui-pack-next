3188d7e05cc2343724868f18b145df3d0c024364
tarchevskyivan <i.tarchevsky@gmail.com>
2025-02-06 12:20:25 +0300

    Перестановки в index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import { NextPage } from 'next'
-import Hero from '@/components/hero/Hero'
 import Accordion from '@/components/accordion/Accordion'
-import FadeIn from '@/components/fadeIn/FadeIn'
-import Meta from '@/components/meta/Meta'
-import CarouselBeyond from '@/components/carouselBeyond/CarouselBeyond'
 import Carousel from '@/components/carousel/Carousel'
-import { MaterialSlider } from '@/components/materialSlider/MaterialSlider'
+import CarouselBeyond from '@/components/carouselBeyond/CarouselBeyond'
+import FadeIn from '@/components/fadeIn/FadeIn'
+import Hero from '@/components/hero/Hero'
 import { images } from '@/components/infiniteMasonry/images'
 import InfiniteMasonry from '@/components/infiniteMasonry/InfiniteMasonry'
+import { MaterialSlider } from '@/components/materialSlider/MaterialSlider'
+import Meta from '@/components/meta/Meta'
+import { NextPage } from 'next'
 
 const HomePage: NextPage = () => {
 	return (
