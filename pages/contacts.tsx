92303d8a1d0c9e59c02c6d86ce32ceaf2483dc9d
tarchevskyivan <i.tarchevsky@gmail.com>
2025-02-07 14:24:55 +0300

    Колонки для контактов в contacts.tsx

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -14,8 +14,18 @@ const ContactsPage: NextPage = () => {
 					<h1 className={`text-4xl font-bold`}>{title}</h1>
 				</main>
 			</FadeIn>
-			<FadeIn className='cont mb-40'>
+			<FadeIn className='cont mb-40 grid grid-cols-2 gap-16'>
 				<ContactForm title={title} />
+				<div className={'flex flex-col gap-6 mt-10'}>
+					<h2 className={'text-3xl font-bold'}>
+						Оставьте заявку, либо свяжитесь с нами
+					</h2>
+					<div className={'flex flex-col gap-4'}>
+						<a href={'mailto:mail@mail.ru'}>mail@mail.ru</a>
+						<a href={'tel:+79000000000'}>+79000000000</a>
+					</div>
+				</div>
+
 			</FadeIn>
 		</>
 	)
