383688b34f3b88cd9a5f35c59d2c3bf76f88561a
tarchevskyivan <i.tarchevsky@gmail.com>
2025-02-10 13:43:11 +0300

    Quiz - положение кнопки submit
    около стрелок навигации для лучшего юзабилити и её адаптация

--- src/components/quiz/StepNavigation.tsx
+++ src/components/quiz/StepNavigation.tsx
@@ -1,5 +1,3 @@
-import FadeIn from '@/components/fadeIn/FadeIn'
-
 import type { FC } from 'react'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 import type { StepNavigationProps } from './quiz.types'
@@ -12,44 +10,44 @@ const StepNavigation: FC<StepNavigationProps> = ({
 	onSubmit,
 	isSubmitting
 }) => (
-	<>
+	<div
+		className={`${currentStep === totalSteps ? 'grid grid-cols-[auto_48px_48px] md:flex md:justify-end md:items-center' : 'flex justify-end items-center'} gap-2`}
+	>
 		{currentStep === totalSteps ? (
-			<FadeIn delay={0.5}>
-				<button
-					type='submit'
-					onClick={onSubmit}
-					className='btn btn-primary'
-					disabled={isSubmitting}
-				>
-					{isSubmitting ? 'Отправка...' : 'Отправить'}
-				</button>
-			</FadeIn>
+			<button
+				type='submit'
+				onClick={onSubmit}
+				className='btn btn-primary'
+				disabled={isSubmitting}
+			>
+				{isSubmitting ? 'Отправка...' : 'Отправить'}
+			</button>
 		) : null}
-		<div className='flex justify-end'>
+
+		<button
+			type='button'
+			onClick={prevStep}
+			className={`btn ${currentStep === 1 ? 'btn-disabled' : 'btn-secondary'}`}
+			disabled={currentStep === 1}
+		>
+			<IoIosArrowBack />
+		</button>
+
+		{currentStep === totalSteps ? (
 			<button
 				type='button'
-				onClick={prevStep}
-				className={`btn ${currentStep === 1 ? 'btn-disabled' : 'btn-secondary'}`}
-				disabled={currentStep === 1}
+				onClick={nextStep}
+				className='btn btn-primary btn-disabled'
+				aria-disabled='true'
 			>
-				<IoIosArrowBack />
+				<IoIosArrowForward />
 			</button>
-			{currentStep === totalSteps ? (
-				<button
-					type='button'
-					onClick={nextStep}
-					className='btn btn-primary btn-disabled'
-					aria-disabled='true'
-				>
-					<IoIosArrowForward />
-				</button>
-			) : (
-				<button type='button' onClick={nextStep} className='btn btn-primary'>
-					<IoIosArrowForward />
-				</button>
-			)}
-		</div>
-	</>
+		) : (
+			<button type='button' onClick={nextStep} className='btn btn-primary'>
+				<IoIosArrowForward />
+			</button>
+		)}
+	</div>
 )
 
 export default StepNavigation
