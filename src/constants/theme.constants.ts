e49b6b270f2fadf366eb905e09c51f158984313e
tarchevskyivan <i.tarchevsky@gmail.com>
2025-03-14 11:53:58 +0300

    Новые темы

--- src/constants/theme.constants.ts
+++ src/constants/theme.constants.ts
@@ -1,5 +1,5 @@
-export const THEME = 'nord'
-export const DARK_THEME = 'business'
+export const THEME = 'winter'
+export const DARK_THEME = 'night'
 export const ind_xs = '0 0 25px 0'
 export const ind_sm = '0 0 50px 0'
 export const ind_md = '0 0 100px 0'
